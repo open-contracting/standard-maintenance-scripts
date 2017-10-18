bc98d7c135154a21493666ef267c1f30456b6f3a
Ben Webb <bjwebb67@googlemail.com>
2017-10-18 09:38:26 +0100

    Disable markdown linting

--- tests/script.sh
+++ tests/script.sh
@@ -7,7 +7,7 @@ flake8 --max-line-length 119
 # MD013 Line length (breaking lines in paragraphs produces longer diffs)
 # MD024 Multiple headers with the same content (see https://github.com/markdownlint/markdownlint/issues/175)
 # MD033 Inline HTML (some files require HTML)
-/tmp/bin/mdl --git-recurse --rules ~MD013,~MD024,~MD033 .
+#/tmp/bin/mdl --git-recurse --rules ~MD013,~MD024,~MD033 .
 
 # Lint and indent JSON and JSON Schema
 curl -s -S $BASEDIR/tests/test_json.py -o /tmp/test_json.py
