283a089ac06131c1b3d6a9441d626fa4a10f7f86
James McKinney <james@slashpoundbang.com>
2017-11-17 22:20:36 -0500

    Move markdownlint configuration to its own file

---
+++ fixtures/mdlrc.rb
@@ -0,0 +1,9 @@
+all
+
+# See https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md#md026---trailing-punctuation-in-header
+rule 'MD026', :punctuation => '.,;:!'
+
+exclude_rule 'MD009' # Trailing spaces (common error frustrates users)
+exclude_rule 'MD013' # Line length (breaking lines in paragraphs produces longer diffs)
+exclude_rule 'MD024' # Multiple headers with the same content (bug https://github.com/markdownlint/markdownlint/issues/175)
+exclude_rule 'MD033' # Inline HTML (some files require HTML)
--- tests/script.sh
+++ tests/script.sh
@@ -4,11 +4,11 @@ set -e
 flake8 --max-line-length 119
 
 # Lint Markdown
-# MD013 Line length (breaking lines in paragraphs produces longer diffs)
-# MD024 Multiple headers with the same content (see https://github.com/markdownlint/markdownlint/issues/175)
-# MD033 Inline HTML (some files require HTML)
-#/tmp/bin/mdl --git-recurse --rules ~MD013,~MD024,~MD033 .
+# See https://github.com/open-contracting/standard-maintenance-scripts/issues/26
+# See http://ocds-standard-development-handbook.readthedocs.io/en/latest/coding/
+# curl -s -S $BASEDIR/fixtures/mdlrc /tmp/mdlrc.rb
+# /tmp/bin/mdl --git-recurse --style /tmp/mdlrc.rb .
 
-# Lint and indent JSON and JSON Schema
+# Validate JSON and JSON Schema
 curl -s -S $BASEDIR/tests/test_json.py -o /tmp/test_json.py
 py.test -rs /tmp/test_json.py
