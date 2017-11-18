c3e19ac70890b3d5c3bd0ff3ee4827d39058491a
James McKinney <james@slashpoundbang.com>
2017-11-17 22:31:26 -0500

    Normalize order of curl flags

--- tests/script.sh
+++ tests/script.sh
@@ -6,9 +6,9 @@ flake8 --max-line-length 119
 # Lint Markdown
 # See https://github.com/open-contracting/standard-maintenance-scripts/issues/26
 # See http://ocds-standard-development-handbook.readthedocs.io/en/latest/coding/
-# curl -s -S $BASEDIR/fixtures/mdlrc /tmp/mdlrc.rb
+# curl -s -S -o /tmp/mdlrc.rb $BASEDIR/fixtures/mdlrc.rb
 # /tmp/bin/mdl --git-recurse --style /tmp/mdlrc.rb .
 
 # Validate JSON and JSON Schema
-curl -s -S $BASEDIR/tests/test_json.py -o /tmp/test_json.py
+curl -s -S -o /tmp/test_json.py $BASEDIR/tests/test_json.py
 py.test -rs /tmp/test_json.py
