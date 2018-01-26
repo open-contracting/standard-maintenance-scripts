b1d63eb8d2166df9fc71aeaf86f40364cc87ed7d
James McKinney <james@slashpoundbang.com>
2018-01-26 13:23:04 -0500

    Add test_csv.py

---
+++ tests/test_csv.py
@@ -0,0 +1,115 @@
+import csv
+import os
+import warnings
+from io import StringIO
+
+
+# Copied from test_json.py.
+def custom_warning_formatter(message, category, filename, lineno, line=None):
+    return str(message).replace(os.getcwd() + os.sep, '')
+
+
+# Copied from test_json.py.
+warnings.formatwarning = custom_warning_formatter
+repo_name = os.path.basename(os.environ.get('TRAVIS_REPO_SLUG', os.getcwd()))
+other_extensions = ('api_extension', 'ocds_performance_failures', 'public-private-partnerships',
+                    'standard_extension_template')
+is_extension = repo_name.startswith('ocds') and repo_name.endswith('extension') or repo_name in other_extensions
+
+
+# Copied from test_json.py.
+def walk(top=os.getcwd()):
+    """
+    Yields all files, except third-party files under `_static` directories.
+    """
+    for root, dirs, files in os.walk(top):
+        for directory in ('.git', '_static', 'fixtures'):
+            if directory in dirs:
+                dirs.remove(directory)
+        for name in files:
+            yield (root, name)
+
+
+# Edited from test_json.py.
+def walk_csv_data(top=os.getcwd()):
+    """
+    Yields all CSV data.
+    """
+    for root, name in walk(top):
+        if name.endswith('.csv'):
+            path = os.path.join(root, name)
+            with open(path, 'r') as f:
+                text = f.read()
+                yield (path, text, csv.DictReader(StringIO(text)))
+
+
+def is_codelist(reader):
+    """
+    Returns whether the CSV is a codelist.
+    """
+    return 'Code' in reader.fieldnames
+
+
+def test_valid():
+    """
+    Ensures all CSV files are valid: no leading or trailing whitespace in cells, no empty columns or rows
+    """
+    errors = 0
+
+    for path, text, reader in walk_csv_data():
+        if is_codelist(reader):
+            width = len(reader.fieldnames)
+            rows = [row for row in reader]
+            columns = []
+
+            for row_index, row in enumerate(rows, 2):
+                if len(row) != width:
+                    errors += 1
+                    warnings.warn('{} has {} not {} columns in row {}'.format(path, len(row), width, row_index))
+                if not any(row.values()):
+                    errors += 1
+                    warnings.warn('{} has empty row {}'.format(path, row_index))
+                else:
+                    for col_index, (header, cell) in enumerate(row.items(), 1):
+                        if col_index > len(columns):
+                            columns.append([])
+
+                        columns[col_index - 1].append(cell)
+
+                        # Extra cells are added to a None columns.
+                        if header is None and isinstance(cell, list):
+                            cells = cell
+                        else:
+                            cells = [cell]
+
+                        for cell in cells:
+                            if cell != cell.strip():
+                                errors += 1
+                                warnings.warn('{} {} "{}" has leading or trailing whitespace at {},{}'.format(
+                                    path, header, cell, row_index, col_index))
+
+            for col_index, column in enumerate(columns, 1):
+                if not any(column):
+                    errors += 1
+                    warnings.warn('{} has empty column {}'.format(path, col_index))
+
+            output = StringIO()
+            writer = csv.DictWriter(output, fieldnames=reader.fieldnames, lineterminator='\n')
+            writer.writeheader()
+            writer.writerows(rows)
+            expected = output.getvalue()
+
+            # TODO: `standard` should be made to conform as well.
+            if is_codelist and text != expected:
+                errors += 1
+                warnings.warn('{} is improperly formatted:\n{}\n{}'.format(path, repr(text), repr(expected)))
+
+    assert errors == 0
+
+
+def test_codelist():
+    """
+    Ensures all codelists files are valid.
+    """
+    pass
+    # TODO: Checks CSV headers for codelists (Code, Title, Description)
