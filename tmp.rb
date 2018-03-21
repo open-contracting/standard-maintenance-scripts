98b5898cdff848810097f30d901da1d82b096a01
James McKinney <james@slashpoundbang.com>
2018-03-21 12:04:37 -0400

    Remove temporary file

--- tmp.rb
+++
@@ -1,5 +0,0 @@
-# TODO: Add script to assign authors of contacts (or people responsible for a country) to:
-
-[
-  Contact.joins("LEFT JOIN addresses ON addressable_id = contacts.id").where('country_code IS NULL OR country_code = ""'),
-  Contact.people.where(company: '').where('author_id IS NOT NULL AND author_id != ""')
