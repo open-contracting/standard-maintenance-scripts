4ceeb358c32b8acae0b96f8da66c912fb67a2518
James McKinney <james@slashpoundbang.com>
2018-12-19 14:32:53 -0500

    Simplify Ruby dependencies

--- tests/install.sh
+++ tests/install.sh
@@ -4,8 +4,4 @@ set -e
 pip install flake8 json-merge-patch jsonref jsonschema pytest requests rfc3987 strict-rfc3339
 
 # Ruby dependencies
-curl -s -S --retry 3 -o /tmp/Gemfile $BASEDIR/fixtures/Gemfile
-# Use same magic incantation as https://github.com/rails/rails/blob/288fbc7ff47b6aae0d5bab978ae16858a425f643/.travis.yml#L30-L31
-gem update --system
-gem install bundler -v 1.15.4
-bundle _1.15.4_ install --gemfile=/tmp/Gemfile --path /tmp/vendor/bundle --binstubs=/tmp/bin
+gem install mdl
