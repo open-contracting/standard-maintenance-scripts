19aa27381ffe4feb394f31553949c2d5420aa11d
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-01-30 13:40:08 -0500

    Download test_readme.py file

--- tests/script.sh
+++ tests/script.sh
@@ -12,4 +12,5 @@ flake8 --max-line-length 119
 # Validate CSV, JSON and JSON Schema
 curl -s -S --retry 3 -o /tmp/test_csv.py $BASEDIR/tests/test_csv.py
 curl -s -S --retry 3 -o /tmp/test_json.py $BASEDIR/tests/test_json.py
+curl -s -S --retry 3 -o /tmp/test_readme.py $BASEDIR/tests/test_readme.py
 py.test -rs --tb=line /tmp/test_csv.py /tmp/test_json.py /tmp/test_readme.py
