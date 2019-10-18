56d9cd5fb871e1d4a90d4fe7e2c1786b4d3a4b52
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-10-18 15:42:55 -0400

    Various fixes. - Use -i="" to work across Linux and macOS. - Repeat -r option instead of creating temporary file. - Make virtualenv assumptions clearer. - Make code comments clearer.

--- scripts/update_requirements.sh
+++ scripts/update_requirements.sh
@@ -2,42 +2,42 @@
 # This follows broadly the approach from
 # http://www.kennethreitz.org/essays/a-better-pip-workflow but with the
 # addition of requirements_dev
-#
-# This assumes you want to use the virtualenv tool,
-# and want to create a virtual environment called ".ve".
-#
-# If using a different process, please make sure to preserve the same "-r" option
-# to freeze as here.
-# This means it is easy to compare changes in lock files between commits and see
-# what libraries have had their versions upgraded and which haven't.
 
-# Delete and recreate a virtualenv to ensure that we don't have any extra
-# packages installed in it
+# This assumes that:
+#
+# * You have already installed the `virtualenv` package
+# * You want a virtual environment in a `.ve` directory at the root of the repository
+#
+# If you instead prefer to use `pyenv`, etc., you will need to substitute different commands.
+
+# Delete and re-create a fresh virtual environment, to eliminate any packages not specified in requirements files.
 rm -rf .ve
 virtualenv --python=python3 .ve
 source .ve/bin/activate
 
+# If you run this script with a `--new-only` option, then the versions of packages in `requirements.txt` will not be
+# upgraded. Instead, only new packages from `requirements.in` and their dependencies will be installed.
 if [[ "$1" == "--new-only" ]]; then
-    # If --new-only is supplied then we install the current versions of
-    # packages into the virtualenv, so that the only change will be any new
-    # packages and their dependencies.
-    pip install -r requirements.txt
     dashupgrade=""
 else
     dashupgrade="--upgrade"
 fi
+
+# It is important to use the `-r` option with `pip freeze` to preserve any comments and the order of requirements,
+# in order to make it easy to compare changes to the requirements.txt file.
+if [[ "$1" == "--new-only" ]]; then
+    pip install -r requirements.txt
+fi
 pip install $dashupgrade -r requirements.in
 pip freeze -r requirements.in > requirements.txt
 
-# Same again for requirements_dev
+# Same as above for requirements_dev.*.
 if [[ "$1" == "--new-only" ]]; then
     pip install -r requirements_dev.txt
 fi
 pip install $dashupgrade -r requirements_dev.in
-cat requirements.in requirements_dev.in > requirements_combined_tmp.in
-pip freeze -r requirements_combined_tmp.in > requirements_dev.txt
-rm requirements_combined_tmp.in
+pip freeze -r requirements.in -r requirements_dev.in > requirements_dev.txt
 
-# Some cleanups needed to remove some things in the locked files that cause problems
-sed -i 's/^-r.*//' requirements.txt requirements_dev.txt
-sed -i 's/pkg-resources==0.0.0//' requirements.txt requirements_dev.txt
+# Remove lines that might occur in the *.txt files that cause problems.
+sed -i='' 's/^-r.*//' requirements.txt requirements_dev.txt
+sed -i='' 's/pkg-resources==0.0.0//' requirements.txt requirements_dev.txt
