39a0111594cbeb736a626812cfeef39ed190a31e
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-11-13 20:33:57 -0500

    Add tests for validity of backticked and single-quoted text in extension readmes, #10

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -1,14 +1,18 @@
+import csv
 import json
 import os
 import re
 import warnings
 from copy import deepcopy
+from io import StringIO
 
 import json_merge_patch
+import jsonref
 import pytest
 import requests
 from jsonschema import FormatChecker
 from jsonschema.validators import Draft4Validator as validator
+from ocdskit.schema import get_schema_fields
 
 
 # Copied from test_json.py.
@@ -56,6 +60,12 @@ def custom_warning_formatter(message, category, filename, lineno, line=None):
 warnings.formatwarning = custom_warning_formatter
 
 
+def read_metadata():
+    path = os.path.join(cwd, 'extension.json')
+    with open(path) as f:
+        return json.load(f)
+
+
 def read_readme():
     path = os.path.join(cwd, 'README.md')
     if os.path.isfile(path):
@@ -70,7 +80,25 @@ def examples():
         try:
             yield i, text, json.loads(text)
         except json.decoder.JSONDecodeError as e:
-            assert False, 'JSON block {} is not valid JSON ({})'.format(i, e)
+            assert False, 'README.md: JSON block {} is not valid JSON ({})'.format(i, e)
+
+
+def patch_schema():
+    # Adapted from test_json.py.
+    def get_dependencies(extension):
+        dependencies = extension.get('dependencies', []) + extension.get('testDependencies', [])
+        for url in dependencies:
+            dependency = requests.get(url).json()
+            schema_url = url.rsplit('/', 1)[0] + '/release-schema.json'
+            json_merge_patch.merge(patched, requests.get(schema_url).json())
+            get_dependencies(dependency)
+
+    patched = deepcopy(schema)
+    get_dependencies(read_metadata())
+    with open(os.path.join(cwd, 'release-schema.json')) as f:
+        json_merge_patch.merge(patched, json.load(f))
+
+    return patched
 
 
 @pytest.mark.skipif(not is_extension, reason='not an extension (test_example_present)')
@@ -88,8 +116,7 @@ def test_example_present():
     readme = read_readme()
 
     # ocds_enquiry_extension has "Example" as text, instead of as a heading.
-    if not re.search(r'\bexamples?\b', readme, re.IGNORECASE) or '```json' not in readme:
-        warnings.warn('README.md expected an example')
+    assert re.search(r'\bexamples?\b', readme, re.IGNORECASE) and '```json' in readme, 'README.md: expected an example'
 
 
 @pytest.mark.skipif(not is_extension, reason='not an extension (test_example_indent)')
@@ -117,20 +144,7 @@ def test_example_valid():
             for value in data.values():
                 set_additional_properties_false(value)
 
-    # Adapted from test_json.py.
-    def get_dependencies(extension):
-        dependencies = extension.get('dependencies', []) + extension.get('testDependencies', [])
-        for url in dependencies:
-            dependency = requests.get(url).json()
-            schema_url = url.rsplit('/', 1)[0] + '/release-schema.json'
-            json_merge_patch.merge(patched, requests.get(schema_url).json())
-            get_dependencies(dependency)
-
-    patched = deepcopy(schema)
-    with open(os.path.join(cwd, 'extension.json')) as f:
-        get_dependencies(json.loads(f.read()))
-    with open(os.path.join(cwd, 'release-schema.json')) as f:
-        json_merge_patch.merge(patched, json.loads(f.read()))
+    patched = patch_schema()
     set_additional_properties_false(patched)
 
     for i, text, data in examples():
@@ -150,4 +164,120 @@ def test_example_valid():
             warnings.warn(json.dumps(error.instance, indent=2, separators=(',', ': ')))
             warnings.warn('{} ({})\n'.format(error.message, '/'.join(error.absolute_schema_path)))
 
-        assert errors == 0, 'JSON block {} is invalid. See warnings below.'.format(i)
+        assert errors == 0, 'README.md: JSON block {} is invalid. See warnings below.'.format(i)
+
+
+@pytest.mark.skipif(not is_extension, reason='not an extension (test_example_indent)')
+def test_example_backticks():
+    exceptions = {
+        # Substring of pattern property.
+        'ocds_exchangeRate_extension': [
+            'CODE',
+        ],
+        # Cross-references to other extensions.
+        'ocds_contract_signatories_extension': [
+            'preferredBidders',
+            'publicAuthority',
+        ],
+        'ocds_metrics_extension': [
+            'RequirementResponse',
+        ],
+        # Changelog entries for non-existent or removed fields.
+        'ocds_bid_extension': [
+            'BidsStatistic.requirementResponses',
+        ],
+        'ocds_budget_projects_extension': [
+            'Project.source',
+            'Project.project',
+        ],
+        'ocds_lots_extension': [
+            'LotDetails',
+        ],
+        'ocds_qualification_extension': [
+            'PreQualification.procurementMethodRationale',
+            'PreQualification.awardCriteriaDetails',
+        ],
+    }
+
+    # Add JSON null, JSON booleans, and a jsonmerge field from OCDS 1.0.
+    literals = {'null', 'true', 'false', 'mergeStrategy'}
+    patterns = set()
+
+    # Add JSON Schema properties.
+    url = 'https://raw.githubusercontent.com/open-contracting/standard/1.1/standard/schema/meta-schema.json'
+    literals.update(requests.get(url).json()['properties'])
+
+    # Add codelist columns.
+    url = 'https://raw.githubusercontent.com/open-contracting/standard-maintenance-scripts/master/schema/codelist-schema.json'  # noqa
+    literals.update(requests.get(url).json()['items']['properties'])
+
+    # Add codelist names.
+    metadata = read_metadata()
+    literals.update(metadata.get('codelists', []))
+
+    # Add JSON paths, field names and definition names..
+    schema = jsonref.JsonRef.replace_refs(patch_schema())
+    for field in get_schema_fields(schema):
+        if 'patternProperties' in field.pointer_components:
+            literal, pattern = re.search(r'^(.*)\(\^?(.+)\$?\)$', field.path).groups()
+            patterns.add(re.compile(r'^' + re.escape(literal) + pattern + r'$'))
+        else:
+            literals.add(field.path)  # e.g. tender.id
+            if len(field.path_components) > 1:
+                literals.add(field.path_components[-1])  # e.g. scale
+            if field.definition_path_components:
+                literals.add(field.definition_path)  # e.g. Lot
+                literals.add('{}.{}'.format(field.definition_path, field.path))  # e.g. Lot.id
+
+    errors = 0
+
+    for text in re.findall(r'`([^`\n]+)`', read_readme(), re.DOTALL):
+        if (text not in literals and not any(re.search(pattern, text) for pattern in patterns)
+                # e.g. `"uniqueItems": true`
+                and not text.startswith('"') and text not in exceptions.get(repo_name, [])):
+            errors += 1
+            warnings.warn('README.md: "{}" term is not in schema'.format(text))
+
+    assert errors == 0, 'README.md: Backtick terms are invalid. See warnings below.'
+
+
+@pytest.mark.skipif(not is_extension, reason='not an extension (test_example_indent)')
+def test_example_codes():
+    metadata = read_metadata()
+
+    exceptions = {
+        # Open codelists.
+        'ocds_enquiry_extension': [
+            'clarifications',
+        ],
+        # Changelog entries for removed codes.
+        'ocds_ppp_extension': [
+            'disqualifiedBidder',
+            'qualifiedBidder',
+        ],
+    }
+
+    literals = set()
+
+    for codelist in ('milestoneStatus', 'releaseTag'):  # can save time, for now, by not downloading all codelists
+        reader = csv.DictReader(StringIO(requests.get('{}/codelists/{}.csv'.format(url_prefix, codelist)).text))
+        for row in reader:
+            literals.add(row['Code'])
+
+    if 'codelists' in metadata:
+        for codelist in metadata['codelists']:
+            with open(os.path.join('codelists', codelist)) as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    literals.add(row['Code'])
+                    if 'Category' in row:
+                        literals.add(row['Category'])
+
+    errors = 0
+
+    for text in re.findall(r"'(\S+)'", read_readme(), re.DOTALL):
+        if text not in literals and text not in exceptions.get(repo_name, []):
+            errors += 1
+            warnings.warn('README.md: "{}" code is not in codelists'.format(text))
+
+    assert errors == 0, 'README.md: Single-quote terms are invalid. See warnings below.'
