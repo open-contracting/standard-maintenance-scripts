3632c14f5fb5442255fae51b7bea8a7453a3333a
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-11-25 13:59:29 -0500

    Add isort to common tests

--- tests/install.sh
+++ tests/install.sh
@@ -1,7 +1,7 @@
 set -e
 
 # Python dependencies
-pip install flake8 json-merge-patch jsonref jsonschema ocdskit 'pytest>=3.6' requests rfc3987 strict-rfc3339
+pip install flake8 isort json-merge-patch jsonref jsonschema ocdskit 'pytest>=3.6' requests rfc3987 strict-rfc3339
 
 # Ruby dependencies
 gem install mdl
--- tests/script.sh
+++ tests/script.sh
@@ -2,6 +2,7 @@ set -e
 
 # Lint Python
 flake8 --max-line-length 119
+isort --line-width 119
 
 # Lint Markdown
 # See https://github.com/open-contracting/standard-maintenance-scripts/issues/26
