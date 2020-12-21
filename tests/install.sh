391de274f2e74ee78d159acc82f69b24dca568e7
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-12-21 18:33:54 -0500

    ci: Add shellcheck

--- tests/install.sh
+++ tests/install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -euo pipefail
 
 pip install flake8 isort jscc json-merge-patch jsonref jsonschema pytest 'ocdskit>=0.2.14' requests
--- tests/script.sh
+++ tests/script.sh
@@ -1,15 +1,16 @@
+#!/usr/bin/env bash
 set -xeuo pipefail
 
 # Ignore version control, untracked files and executable scripts.
-! find . -type f \! -perm 644 \! -path '*/.git/*' \! -path '*/.tox/*' \! -path '*/__pycache__/*' \! -path '*/cache/*' \
+find . -type f \! -perm 644 \! -path '*/.git/*' \! -path '*/.tox/*' \! -path '*/__pycache__/*' \! -path '*/cache/*' \
   \! -path '*/node_modules/*' \! -path '*/script/*' \! -name '*.sh' \! -name '*-cli' \! -name 'manage.py' \
-  -o -type d \! -perm 755 \! -path '*/deploy/cache/*' | grep .
+  -o -type d \! -perm 755 \! -path '*/deploy/cache/*' | grep . && exit 1
 
 flake8 . --max-line-length 119
 
 isort . --check-only --ignore-whitespace --line-width 119
 
-curl -s -S --retry 3 -o /tmp/test_csv.py $BASEDIR/tests/test_csv.py
-curl -s -S --retry 3 -o /tmp/test_json.py $BASEDIR/tests/test_json.py
-curl -s -S --retry 3 -o /tmp/test_readme.py $BASEDIR/tests/test_readme.py
+curl -s -S --retry 3 -o /tmp/test_csv.py "$BASEDIR"/tests/test_csv.py
+curl -s -S --retry 3 -o /tmp/test_json.py "$BASEDIR"/tests/test_json.py
+curl -s -S --retry 3 -o /tmp/test_readme.py "$BASEDIR"/tests/test_readme.py
 py.test -rs --tb=line /tmp/test_csv.py /tmp/test_json.py /tmp/test_readme.py
