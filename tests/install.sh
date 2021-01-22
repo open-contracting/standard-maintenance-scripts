623c96fc39e5a5fea33419b56f721aa72711b837
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-01-22 18:12:39 -0500

    tests: Upgrade isort

--- tests/install.sh
+++ tests/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-pip install flake8 isort jscc json-merge-patch jsonref jsonschema pytest 'ocdskit>=0.2.14' requests
+pip install flake8 'isort>=5' jscc json-merge-patch jsonref jsonschema pytest 'ocdskit>=0.2.14' requests
