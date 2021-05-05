24e702308d4307de38b58de36176ab51a67a04e5
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-05-05 19:27:53 -0400

    tests: Use tuple

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -30,7 +30,7 @@ def test_csv_valid():
                     'CSV files are invalid. See warnings below.')
 
 
-@pytest.mark.skipif(repo_name in 'pelican-backend', reason='cached upstream file')
+@pytest.mark.skipif(repo_name in ('pelican-backend',), reason='cached upstream file')
 def test_valid():
     """
     Ensures all CSV files are valid: no empty rows or columns, no leading or trailing whitespace in cells, same number
@@ -94,7 +94,7 @@ def test_valid():
     assert errors == 0, 'One or more codelist CSV files are invalid. See warnings below.'
 
 
-@pytest.mark.skipif(repo_name in 'pelican-backend', reason='not a codelist')
+@pytest.mark.skipif(repo_name in ('pelican-backend',), reason='not a codelist')
 def test_codelist():
     """
     Ensures all codelists files are valid against codelist-schema.json.
