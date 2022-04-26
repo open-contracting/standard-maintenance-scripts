dd0c101d846f162c78b29df60d93bda10d8b044f
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-04-26 17:57:25 -0400

    test_requirements: Add support for additional requirements files

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -213,6 +213,7 @@ def check_requirements(path, *requirements_files, dev=False, ignore=()):
 
     ignore = list(ignore) + os.getenv('STANDARD_MAINTENANCE_SCRIPTS_IGNORE', '').split(',')
     extras = os.getenv('STANDARD_MAINTENANCE_SCRIPTS_EXTRAS', '').split(',')
+    requirements_files += tuple(os.getenv('STANDARD_MAINTENANCE_SCRIPTS_FILES', '').split(','))
 
     # Collect the modules that are imported.
     imports = defaultdict(set)
