610a00ba8e0d5b455327e77ee2a881dd1f41d7b0
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-10-28 16:16:01 -0400

    test: Upgrade jscc for jsonref 1.0 compatibility

--- tests/install.sh
+++ tests/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-pip install flake8 'isort>=5' jscc json-merge-patch 'jsonref>=1' jsonschema pytest 'ocdskit>=1' requests
+pip install flake8 'isort>=5' 'jscc>=0.1' json-merge-patch 'jsonref>=1' jsonschema pytest 'ocdskit>=1' requests
