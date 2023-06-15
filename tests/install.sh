c3768fa38fd901848431031a185194fbdeaa031b
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-06-15 00:01:23 -0400

    chore: shfmt

--- tests/install.sh
+++ tests/install.sh
@@ -2,4 +2,3 @@
 set -euo pipefail
 
 pip install flake8 flake8-comprehensions 'isort>=5' 'jscc>=0.2' json-merge-patch 'jsonref>=1' jsonschema pytest 'ocdskit>=1' requests rfc3339-validator rfc3986-validator
-
