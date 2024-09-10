803fa7b3eda2fafc601cacc06b5df5637c9f4517
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-09-10 14:46:01 -0400

    chore: Use ruff.toml

--- tests/script.sh
+++ tests/script.sh
@@ -8,6 +8,6 @@ find . -type f \! -perm 644 \! -path '*/.git/*' \! -path '*/.ruff_cache/*' \! -p
     \! -path '*/script/*' \! -name '*.sh' \! -name '*-cli' \! -name 'manage.py' \! -name 'run.py' \
     -o -type d \! -perm 755 \! -path '*/deploy/cache/*' | grep . && exit 1
 
-ruff check . --select E,C4,F,I,W
+ruff check . --select E,C4,F,I,W --config /tmp/ruff.toml
 
 pytest -rs --tb=line /tmp/test_csv.py /tmp/test_json.py /tmp/test_readme.py # test_requirements.py is opt-in
