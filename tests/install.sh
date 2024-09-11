68cf9ee60bd0586497013598186a9ddf6c9ccd57
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-09-10 22:58:38 -0400

    test: Use key=value instead of ruff.toml

--- tests/install.sh
+++ tests/install.sh
@@ -5,8 +5,6 @@ set -eu
 pip install ruff jscc json-merge-patch jsonref jsonschema ocdskit packaging pytest requests rfc3339-validator \
     rfc3986-validator setuptools
 
-echo "line-length = 119" > /tmp/ruff.toml
-
 curl -s -S --retry 3 -o /tmp/test_csv.py "$BASEDIR"/tests/test_csv.py
 curl -s -S --retry 3 -o /tmp/test_json.py "$BASEDIR"/tests/test_json.py
 curl -s -S --retry 3 -o /tmp/test_readme.py "$BASEDIR"/tests/test_readme.py
