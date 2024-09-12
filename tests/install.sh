84b92f046c07c0667471c5b77e269d258ed2eb6f
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-09-12 14:41:47 -0400

    chore: Lint shell

--- tests/install.sh
+++ tests/install.sh
@@ -3,7 +3,7 @@
 set -eu
 
 pip install ruff jscc json-merge-patch jsonref jsonschema ocdskit packaging pytest requests rfc3339-validator \
-    rfc3986-validator setuptools tomli  # Python 3.10 or less
+    rfc3986-validator setuptools tomli # Python 3.10 or less
 
 curl -s -S --retry 3 -o /tmp/test_csv.py "$BASEDIR"/tests/test_csv.py
 curl -s -S --retry 3 -o /tmp/test_json.py "$BASEDIR"/tests/test_json.py
